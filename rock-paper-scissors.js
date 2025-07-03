a2819e5d074085dcc372fd644dc1040a06149367
Kwame James <kwamekamau@gmail.com>
2025-07-03 00:38:16 -0400

    Begin writing pseudocode in rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,39 @@
 console.log("works");
+
+// Create a function called getComputerChoice that
+
+    // randomly chooses a number value,
+
+    // divides the possible values into three equal ranges and
+
+    // returns a string value of "rock", "paper", or "scissors" depending on what
+    // range the number falls in.
+
+// Create a function called getHumanChoice that
+
+    // prompts the user to input a string ("rock", "paper", or "scissors")
+
+    // and returns the string input by the user.
+
+// Create a variable, humanScore, and initialize it to 0.
+
+// Create a variable, computerScore, and initialize it to 0.
+
+// Create a function, playRound, with two parameters humanChoice and
+// computerChoice.
+
+    // To make humanChoice functionally case insensitive, convert it to lowercase,
+
+    // then compare the two variables. If they are strictly equal,
+
+        // log to console "Tie! Try again."
+
+    // Or else, if humanChoice is "rock",
+
+        // then if computer choice is "paper",
+
+            // log to console "Sorry, loser, paper wraps rock."
+
+        // Or else, 
+
+            // log to console "Yes! Your rock smashes scissors."
